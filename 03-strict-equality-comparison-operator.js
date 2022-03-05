15c06504cd99c2667b776fc9246cae31dac1fcd7
Christian Boyle <mail@christianboyle.com>
2022-03-05 11:12:25 -0600

    add 03-strict-equality-comparison-operator

---
+++ 03-strict-equality-comparison-operator.js
@@ -0,0 +1,19 @@
+// loose equality comparison
+
+1 == '1'; // true
+
+0 == ''; // true
+
+null == undefined; // true
+
+[] == ''; // true
+
+// strict equality comparison
+
+1 === '1'; // false
+
+0 === ''; // false
+
+null === undefined; // false
+
+[] === ''; // false
