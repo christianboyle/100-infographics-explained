9bf858764fa01af3fa99a76e4f72c25fe01c4d42
Christian Boyle <mail@christianboyle.com>
2022-03-20 07:42:07 -0500

    add 18-get-the-last-items-of-an-array

---
+++ 18-get-the-last-items-of-an-array.js
@@ -0,0 +1,5 @@
+const array = [1, 2, 3, 4, 5]
+
+const lastElement = array.slice(-1) // [5]
+const lastTwoElements = array.slice(-2) // [4, 5]
+const lastThreeElements = array.slice(-3) // [3, 4, 5]
