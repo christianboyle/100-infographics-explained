8740c6c54387ee47d13bccb4aabd1e730f5169b5
Christian Boyle <mail@christianboyle.com>
2022-04-23 11:56:22 -0500

    add 31-anatomy-of-a-for-loop

---
+++ 31-anatomy-of-a-for-loop.js
@@ -0,0 +1,13 @@
+/*
+for (let loopVariable = startValue); loopVariable < endValue; loopVariable++) {
+  someArray[loopVariable];
+  
+  doSomethingWith(loopVariable)
+}
+*/
+
+let array = [1, 2, 3, 4, 5]
+
+for (let i = 0; i < array.length; i++) {
+  array[i] // 1, 2, 3, 4, 5
+}
