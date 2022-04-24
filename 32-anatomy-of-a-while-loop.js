0449f4afbfcb206e633ffa44307c8ad65fd912bf
Christian Boyle <mail@christianboyle.com>
2022-04-24 07:23:57 -0500

    add 32-anatomy-of-a-while-loop

---
+++ 32-anatomy-of-a-while-loop.js
@@ -0,0 +1,25 @@
+/*
+while (condition === true) {
+  doAction();
+}
+*/
+
+let text = ''
+let i = 0
+while (i < 10) {
+  text += '<br>The number is ' + i
+  i++
+}
+
+/*
+The number is 0
+The number is 1
+The number is 2
+The number is 3
+The number is 4
+The number is 5
+The number is 6
+The number is 7
+The number is 8
+The number is 9
+*/
