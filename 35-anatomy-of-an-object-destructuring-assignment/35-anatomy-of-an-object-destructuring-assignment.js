f78211cfa5410c14fa7345ad4602d166839bff65
Christian Boyle <mail@christianboyle.com>
2022-05-07 10:39:04 -0500

    add 35-anatomy-of-an-object-destructuring-assignment

---
+++ 35-anatomy-of-an-object-destructuring-assignment/35-anatomy-of-an-object-destructuring-assignment.js
@@ -0,0 +1,9 @@
+let a, b, rest
+;[a, b] = [10, 20]
+console.log(a) // 10
+console.log(b) // 20
+
+;[a, b, ...rest] = [10, 20, 30, 40, 50]
+console.log(a) // 10
+console.log(b) // 20
+console.log(rest) // [30, 40, 50]
