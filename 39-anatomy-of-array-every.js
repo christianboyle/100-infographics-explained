3edd2b6fc3dd5c85923a11f3abc4b4efe39dbe29
Christian Boyle <mail@christianboyle.com>
2022-05-22 07:44:58 -0500

    add 40-how-to-format-the-output-of-json-stringify

--- 39-anatomy-of-array-every.js
+++ 39-anatomy-of-array-every.js
@@ -4,4 +4,8 @@ const isBelowThreshold = (currentValue) => currentValue < 40
 
 const array1 = [1, 30, 39, 29, 10, 13]
 
-console.log(array1.every(isBelowThreshold))
+const array2 = [41]
+
+console.log(array1.every(isBelowThreshold)) // true
+
+console.log(array2.every(isBelowThreshold)) // false
---
+++ 40-how-to-format-the-output-of-json-stringify.js
@@ -0,0 +1,10 @@
+const formattedJson = JSON.stringify(
+  {
+    handle: '@christianboyle',
+    mission: 'teach everyone JS'
+  },
+  null,
+  '   '
+)
+
+console.log(formattedJson)
