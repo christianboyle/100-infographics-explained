0244f3ac57fc0ebd372e5f54804b3f8a9331f382
Christian Boyle <mail@christianboyle.com>
2022-05-30 06:35:59 -0500

    add 43-template-literals

---
+++ 43-template-literals.js
@@ -0,0 +1,5 @@
+const formatMessage = (one, two) => {
+  return `Argument one is: ${one}; Argument two is: ${two}`
+}
+
+console.log(formatMessage('apple', 'banana')) // Argument one is: apple; Argument two is: banana
