34c25fbce0a1a3c7c5335b4190b9fcd222641bd5
Christian Boyle <mail@christianboyle.com>
2022-06-08 10:40:46 -0500

    add 46-use-array-includes-instead-of-index-of

---
+++ 46-use-array-includes-instead-of-index-of.js
@@ -0,0 +1,9 @@
+const array = [1, 2, 3, 4, 5]
+
+// old approach: indexOf
+
+const containsFour = array.indexOf(4) > -1 // true
+
+// new approach: array.includes
+
+const containsFive = array.includes(5) // true
