097e3b356aaf0f668e3d5da510f481e4d6c75cf5
Christian Boyle <mail@christianboyle.com>
2022-07-10 09:43:51 -0500

    add 66-how-to-reverse-a-string

---
+++ 66-how-to-reverse-a-string.js
@@ -0,0 +1,9 @@
+const reverseString = (str) => {
+  if (!str || str.length < 2) {
+    return str
+  }
+
+  return str.split('').reverse().join('')
+}
+
+console.log(reverseString('something')) // gnihtemos
